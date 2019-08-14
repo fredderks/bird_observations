4e7790276f0345cfba4b9de0c5604cf376fe9d9e
Fred Derks <frederique93@gmail.com>
2019-08-14 14:33:41 +0200

    Included functionality for script to be ran multiple times per day

--- publish_bird_obs_shiny.R
+++ publish_bird_obs_shiny.R
@@ -1,15 +1,13 @@
-rm(list=ls())
-library(ggplot2);library(dplyr);library(ggpubr);library(zoo);library(tidyr);library(data.table);library(shiny);library(leaflet);library(lubridate);library(magrittr);library(stringr);library(purrr);library(rsconnect);library(rvest);library(Hmisc);library(tcltk);library(rsconnect)
+library(tidyverse);library(ggpubr);library(zoo);library(data.table);library(shiny);library(rsconnect);library(rvest);library(Hmisc);library(tcltk)
 setwd("E:/Google Drive/Coding/GitHub/bird_observations")
 #### Scrape data for all LIFER species observed in a certain province today ####
-# userid <- 130065; province <- 0; rarity <- 1 ; oldfile <- "LIFER_obs_0_2019-06-27.rds"
 
 source("scrape_lifer_obs.R")
 sink("log.txt", append = TRUE, split = TRUE)
 print("")
 print(Sys.time())
-file <- paste(sep="", "LIFER_obs_0_",today()-1,".rds")
-ScrapeLiferObs(130065,0,1,file)
+# file <- paste(sep="", "LIFER_obs_0_",today()-1,".rds") # Retrieve yesterday's file
+file <- list.files(pattern = ".rds") %>% max() # Retrieve last file produced
+ScrapeLiferObs(0,1,file)
 deployApp(appDir = getwd(),appFiles = c(paste(sep="","LIFER_obs_0_",today(),".rds"),"www","styles.css","app.R"), forceUpdate = TRUE)
 sink()
-  
