365186c3937e72e728121af250b44903449f9458
Fred Derks <frederiquederks@gmail.com>
2020-01-21 22:16:16 +0100

    Adds a count of observations displayed onscreen
    Also commented some code

--- app.R
+++ app.R
@@ -24,9 +24,13 @@ provinces <- c("All Provinces" = "", "Drenthe" = "(DR)", "Flevoland" = "(FL)", "
 
 #### Define UI for application that draws a map plotting bird observations ####
 ui <- navbarPage(span(tags$i(class = "fas fa-binoculars fa-lg"),style = "font-size:140%;"), windowTitle = "Observations Map",
+  # ---- Interactive Map Page ----
+    # Page container
     tabPanel(span(tags$i(class = "fas fa-map-marked-alt fa-lg"),
                 "Interactive Observations Map",style = "font-size:130%;"),
+        # Map container
         div(class = "outer",
+            # Header
             tags$head(
                 # Include our custom CSS
                 includeCSS("styles.css"),
@@ -34,29 +38,37 @@ ui <- navbarPage(span(tags$i(class = "fas fa-binoculars fa-lg"),style = "font-si
                 tags$link(rel = "shortcut icon", href = "parroticon.png"),
                 tags$script(src = "https://kit.fontawesome.com/0d5117e2fa.js")
                 ),
-        leafletOutput("mymap", width = "100%", height = "100%"),
-        absolutePanel(top = 10, left = 70, width = 300,
-                      wellPanel(
-                          sliderInput("time", p(tags$i(class = "fas fa-calendar-alt"),"Date of observation"), 
-                                      floor_date(min(provdata$date),"hours"), 
-                                      ceiling_date(max(provdata$date),"hours"),
-                                      value = c(floor_date(min(provdata$date)),ceiling_date(max(provdata$date),"hours")),
-                                      step = 3600, ticks = F),
-                          textInput("userid",  p(tags$i(class = "fas fa-id-badge"),"Waarneming.nl User ID"), value = "130065", width = 170),
-                          radioButtons("lifer","",
-                                       choices = list("Only show LIFERs" = 1, "Show all observations" = 2), 
-                                       selected = 2)
-                          ), 
-                      style = "opacity: 0.92" # https://shiny.rstudio.com/gallery/absolutely-positioned-panels.html
+            # Map
+            leafletOutput("mymap", width = "100%", height = "100%"),
+            
+            # Left input panel
+            absolutePanel(top = 10, left = 70, width = 300,
+                          wellPanel(
+                              sliderInput("time", p(tags$i(class = "fas fa-calendar-alt"),"Date of observation"), 
+                                          floor_date(min(provdata$date),"hours"), 
+                                          ceiling_date(max(provdata$date),"hours"),
+                                          value = c(floor_date(min(provdata$date)),ceiling_date(max(provdata$date),"hours")),
+                                          step = 3600, ticks = F),
+                              textInput("userid",  p(tags$i(class = "fas fa-id-badge"),"Waarneming.nl User ID"), value = "130065", width = 170),
+                              radioButtons("lifer","",
+                                           choices = list("Only show LIFERs" = 1, "Show all observations" = 2), 
+                                           selected = 2),
+                              textOutput("count_obs")
+                              ), 
+                          style = "opacity: 0.92" # https://shiny.rstudio.com/gallery/absolutely-positioned-panels.html
+                          )
             )
-        )
-    ),
+        ),
+  
+  # ---- Database Explorer Page ----
+    # Page container
     tabPanel(span(tags$i(class = "fas fa-database fa-lg"),
                 "Data Explorer", style = "font-size:130%;"),
+             # Inputs
              fluidRow(
                  column(3, selectInput("province", "Province", choices = provinces, multiple = TRUE),
-                        p(tags$i(class = "fas fa-edit"),glue('Last updated at: {file.info(file)$mtime}'))
-                        ),
+                          p(tags$i(class = "fas fa-edit"),glue('Last updated at: {file.info(file)$mtime}'))
+                          ),
                  column(5, sliderInput("date", "Date of observation", 
                                        floor_date(min(provdata$date),"hours"), 
                                        ceiling_date(max(provdata$date),"hours"),
@@ -74,17 +86,20 @@ ui <- navbarPage(span(tags$i(class = "fas fa-binoculars fa-lg"),style = "font-si
                  column(2, h4(a("Code is available on GitHub ",tags$i(class = "fab fa-github"), 
                                href = "https://github.com/fredderks/bird_observations"))
                  )
-             ),
-             # hr(),
-             DT::dataTableOutput("obstable")
-             
+              ),
+            # Data Table
+            # hr(),
+            DT::dataTableOutput("obstable")
     )
 )
 
 #### Define server logic required to map bird observations ####
 server <- function(input, output, session) {
     session$onSessionEnded(stopApp)
-
+  
+  # ---- Interactive Map Logic ----
+    
+    # Retrieve a list of all species a user has already observed (lifers), using input from the panel
     lifelist <- reactive({
         tryCatch(
             {lifelist.page <- read_html(paste(sep = "","https://waarneming.nl/users/",input$userid,
@@ -98,6 +113,7 @@ server <- function(input, output, session) {
         )
     })
     
+    # Filters all scraped data to a list only including lifer observations for that user
     liferdata <- reactive({
         if (input$lifer == 1) {
            provdata[!provdata$species %in% lifelist(), ] # https://stackoverflow.com/questions/15227887
@@ -106,12 +122,14 @@ server <- function(input, output, session) {
         }
     })
     
+    # Filer data to the time-period selected in the input panel
     filteredData <- reactive({
         from <- input$time[1]
         till <- input$time[2]
         liferdata() %>% filter(date >= from & date <=  till) %>% arrange(match(rarity, c("algemeen", "vrij algemeen", "zeldzaam", "zeer zeldzaam")))
     })
     
+    # Generate a label for each observation
     labelcontent <- reactive({
         paste(sep = "",
               "<b>",filteredData()$species,"</b>&emsp;[",filteredData()$rarity,"]",
@@ -120,6 +138,7 @@ server <- function(input, output, session) {
               "<br/><a href=",filteredData()$link," target='_blank'>",filteredData()$link,"</a>")
     })
     
+    # Prepare leaflet map
     output$mymap <- renderLeaflet({
         leaflet() %>%
             addTiles() %>%
@@ -131,6 +150,7 @@ server <- function(input, output, session) {
                                                setView = FALSE))
     })
     
+    # Fill map with data and add markers and layers control
     observe({
         leafletProxy("mymap", data = filteredData()) %>%
             clearMarkers() %>%
@@ -139,8 +159,26 @@ server <- function(input, output, session) {
                              options = layersControlOptions(collapsed = FALSE),
                              overlayGroups = ~rarity)
     })
+
+    # count how many observations are being displayed
+    obs_count <- reactive({
+      # retrieve the rarity groups that are selected in the right pane
+      selected_groups <- req(input$mymap_groups)
+      selected_groups <- selected_groups[2:length(selected_groups)]
+      
+      # filter the data based on the groups selected and return a count of observations
+      displayed_data <- filter(filteredData(),
+                           rarity %in% selected_groups
+                           )
+      count(displayed_data)[[1]]
+    })
     
-    #### DataTable
+    # Render text: number of observations displayed
+    output$count_obs <- renderText({ 
+      paste(obs_count(), "observations are being displayed")
+    })
+    
+  # ---- Data Explorer Logic ----
     output$obstable <- DT::renderDataTable({
         df <- provdata %>% 
             filter( date >= input$date[1],
@@ -157,5 +195,5 @@ server <- function(input, output, session) {
     })
 }
 
-# Run the application 
+#### Run the application ####
 shinyApp(ui = ui, server = server)
